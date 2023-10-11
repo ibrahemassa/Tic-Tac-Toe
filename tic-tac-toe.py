5ed2d3cea5d68ace0b632818af8e82cc170266fb
Ibrahim Faress <120706693+ibrahemassa@users.noreply.github.com>
2023-10-11 11:00:07 +0300

    Add playing against CPU

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -16,6 +16,13 @@ def column_equal(my_2d_list, column_index):
     column_values = [row[column_index] for row in my_2d_list]
     return all(element == column_values[0] for element in column_values)
 
+def random_element(my_2d_list):
+    matching_coordinates = [(i, j) for i, row in enumerate(my_2d_list) for j, val in enumerate(row) if val == ' ']
+    
+    if matching_coordinates:
+        random_coordinate = random.choice(matching_coordinates)
+        return[random_coordinate[0], random_coordinate[1]]
+
 again = 1
 
 while again == 1:
@@ -35,42 +42,86 @@ while again == 1:
 
     win = False
     turn = 1
+    Ai = int(input("How many players: "))
+    if Ai != 1:
+      while win != True and any(' ' in row for row in placed):
+          if turn%2 :
+              cur = 'X'
+          else:
+              cur = 'O'
 
-    while win != True and any(' ' in row for row in placed):
-        if turn%2 :
-            cur = 'X'
-        else:
-            cur = 'O'
+          print(f"It's {cur} turn")
+          row = int(input(f'row: '))
+          col = int(input(f'column: '))
 
-        print(f"It's {cur} turn")
-        row = int(input(f'row: '))
-        col = int(input(f'column: '))
+          if placed[row-1][col-1] == " ":
+              placed[row-1][col-1] = cur
+          else:
+              print("\nAlready taken!!\nTurn skipped!!!")
+              turn += 1
+              continue
 
-        if placed[row-1][col-1] == " ":
-            placed[row-1][col-1] = cur
-        else:
-            print("\nAlready taken!!\nTurn skipped!!!")
-            turn += 1
-            continue
+          grid = f'''
+         1    2    3
+        ---  ---  ---
+      1 | {placed[0][0]} | {placed[0][1]} | {placed[0][2]} |
+        --- --- ---
+      2 | {placed[1][0]} | {placed[1][1]} | {placed[1][2]} |
+        --- --- ---
+      3 | {placed[2][0]} | {placed[2][1]} | {placed[2][2]} |
+        --- --- ---
+      '''
 
-        grid = f'''
-       1    2    3
-      ---  ---  ---
-    1 | {placed[0][0]} | {placed[0][1]} | {placed[0][2]} |
-      --- --- ---
-    2 | {placed[1][0]} | {placed[1][1]} | {placed[1][2]} |
-      --- --- ---
-    3 | {placed[2][0]} | {placed[2][1]} | {placed[2][2]} |
-      --- --- ---
-    '''
+          print(grid)
 
-        print(grid)
+          if row_equal(placed[row-1]) or slope(placed) or column_equal(placed, col-1):
+              win = True
+              break
 
-        if row_equal(placed[row-1]) or slope(placed) or column_equal(placed, col-1):
-            win = True
-            break
+          turn += 1
+    else:
+      while win != True and any(' ' in row for row in placed):
+          if turn%2 :
+              cur = 'X'
+          else:
+              cur = 'O'
+          if cur == 'X':
+            print(f"It's {cur} turn")
+            row = int(input(f'row: '))
+            col = int(input(f'column: '))
+              
 
-        turn += 1
+            if placed[row-1][col-1] == " ":
+                placed[row-1][col-1] = cur
+            else:
+                print("\nAlready taken!!\nTurn skipped!!!")
+                turn += 1
+                continue
+            
+          else:
+              re = random_element(placed)
+              row = re[0] + 1
+              col = re[1] + 1
+              placed[row - 1][col - 1] = 'O'
+
+          grid = f'''
+         1    2    3
+        ---  ---  ---
+      1 | {placed[0][0]} | {placed[0][1]} | {placed[0][2]} |
+        --- --- ---
+      2 | {placed[1][0]} | {placed[1][1]} | {placed[1][2]} |
+        --- --- ---
+      3 | {placed[2][0]} | {placed[2][1]} | {placed[2][2]} |
+        --- --- ---
+      '''
+
+          print(grid)
+
+          if row_equal(placed[row-1]) or slope(placed) or column_equal(placed, col-1):
+              win = True
+              break
+
+          turn += 1
 
     if win:
         print(f'The winner is {cur}!!')
